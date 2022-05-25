45bfb805ded65df7614c480e133f7fc85d7571f0
SAMUEL MARCIANO <samuel.marciano@senacsp.edu.br>
2022-05-24 22:16:18 -0300

    list boks quase pronta

--- src/page/editbook/editbook.jsx
+++ src/page/editbook/editbook.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {useParams} from 'react-router-dom'
-import Editform from 'components/Form/Form'
+import Newbooks from 'page/Newbooks/newbooks';
 
 
 
@@ -10,7 +10,7 @@ const Editbook=() =>{
 
     return(
         <>
-            <Editform id={id? Number.parseInt(id,10) : null} />    
+            <Newbooks id={id? Number.parseInt(id,10) : null} />    
         </>
     )
 }
